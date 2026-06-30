45cdd88cf3758f3b9c816dcde188fd52a6ba3ad8
Nickson Kiyoshi <phoneasus158@gmail.com>
2026-06-30 13:58:05 +0700

    deleting port number, because we're using a domanin now

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,7 @@ const { env, port } = require('./core/config');
 const logger = require('./core/logger')('app');
 const server = require('./core/server');
 
-const app = server.listen(port, (err) => {
-  if (err) {
-    logger.fatal(err, 'Failed to start the server.');
-    process.exit(1);
-  } else {
-    logger.info(`Server runs at port ${port} in ${env} environment`);
-  }
-});
+const app = server;
 
 process.on('uncaughtException', (err) => {
   logger.fatal(err, 'Uncaught exception.');
