8769039a7759fc089962b99149a7fbe2bed9873d
Nickson Kiyoshi <phoneasus158@gmail.com>
2026-06-30 14:14:05 +0700

    debuging api

---
+++ backend/src/api/index.js
@@ -0,0 +1,3 @@
+const app = require('../core/server');
+
+module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,14 @@ const { env, port } = require('./core/config');
 const logger = require('./core/logger')('app');
 const server = require('./core/server');
 
-const app = server;
+const app = server.listen(port, (err) => {
+  if (err) {
+    logger.fatal(err, 'Failed to start the server.');
+    process.exit(1);
+  } else {
+    logger.info(`Server runs at port ${port} in ${env} environment`);
+  }
+});
 
 process.on('uncaughtException', (err) => {
   logger.fatal(err, 'Uncaught exception.');
