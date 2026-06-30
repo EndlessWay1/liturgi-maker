daa1775ada954c5c9c201a2a18447558af2a313b
Nickson Kiyoshi <phoneasus158@gmail.com>
2026-06-30 14:47:02 +0700

    fix pino problems

--- backend/src/core/logger.js
+++ backend/src/core/logger.js
@@ -1,31 +1,29 @@
 const pino = require('pino');
 
-// Print the log to STDOUT with colors and formatting for easier read
-const pinoPretty = {
-  target: 'pino-pretty',
-  options: {
-    colorize: true,
-    crlf: true,
-    translateTime: 'SYS:standard',
-  },
-};
-
-module.exports = (appName) =>
-  pino(
-    {
-      name: appName,
-      formatters: {
-        // Specify the level name instead of its integer value.
-        level: (label) => ({
-          level: label.toUpperCase(),
-        }),
-      },
-      timestamp: pino.stdTimeFunctions.isoTime,
-      level: 'trace',
-      redact: {
-        paths: ['password', '*.password', 'token', 'authorization'],
-        censor: '[REDACTED]',
-      },
+module.exports = (appName) => {
+  const options = {
+    name: appName,
+    formatters: {
+      level: (label) => ({ level: label.toUpperCase() }),
     },
-    pino.transport(pinoPretty)
-  );
+    timestamp: pino.stdTimeFunctions.isoTime,
+    level: 'trace',
+    redact: {
+      paths: ['password', '*.password', 'token', 'authorization'],
+      censor: '[REDACTED]',
+    },
+  };
+
+  return process.env.NODE_ENV === 'production'
+    ? pino(options)
+    : pino(
+        options,
+        pino.transport({
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+          },
+        })
+      );
+};
